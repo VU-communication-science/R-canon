256bb667e8c4413e4eef2d4e9078bf2ac06b0c7b
Kasper Welbers <kasperwelbers@gmail.com>
2024-08-27 13:55:29 +0200

    updated demo data

--- data/generate_demo_data.r
+++ data/generate_demo_data.r
@@ -1,45 +1,36 @@
 # Generates broad purpose demo data for explaining tidyverse and statistics
 
 # whether watching a movie about journalism can affect trust in journalists.
-# - Two repeated measures: before and after the intervention
+# - Two repeated measures: trust before and after the intervention
 # - Three groups: control, positive about journalism, negative about journalism 
-
-
-# Data is about participants in a fictional experiment, which aims to measure whether giving participants information about how
-# journalism works affects their trust in journalists. 
-# - Two repeated measures: before and after the intervention
-# - Three groups: control, leaflet, seminar
-# - Three experimental groups
-# - demographics: gender, age
-
-# Effect of seminar about how journalism works on 
-# control, leaflet, seminar
-# participant_id, age, np_subscription, experimental_group, trust_t1,
+# - Other variables: age, newspaper subscription, news consumption
 
 library(sjPlot)
 library(tidyverse)
-
-
-
 set.seed(1)
 
-
-
 # number of participants. Make sure to use multiple of 3 for equal distribution of groups
-n <- 300
+n <- 600
 
 # simulate age
 age <- round(runif(n, 18, 65))
 
 # assign groups in repetitions to ensure equal distribution (data is random anyway)
-experimental_group <- rep(1:3, length.out=n) |> factor(labels = c("control", "negative", "positive"))
+experiment_group <- rep(1:3, length.out=n) |> factor(labels = c("control", "negative", "positive"))
 
 # simulate subscription to have likelihood increase with age, using a logistic function
-subscription_odds <- 1 / (1 + exp(-0.06* age + 2))
+subscription_odds <- 1 / (1 + exp(-0.06*age + 2))
 np_subscription <- rbinom(n, 1, subscription_odds)
 
 subscription_prob = subscription_odds / (1 + subscription_odds)
-plot(age, subscription_prob)
+plot(age ~ as.factor(np_subscription))
+
+# create variable for average hours of news consumption per week, and make it so that it's higher for people with
+# newspaper subscription, but people without subscription have higher variance (for showing levene's test)
+news_consumption <- rnorm(n, mean = 8 + np_subscription*4, sd = 3 - np_subscription*1.5)
+news_consumption[news_consumption < 0] <- 0
+news_consumption = (news_consumption)
+plot(news_consumption ~ as.factor(np_subscription))
 
 # simulate trust_t1 to increase slightly with age. Force 10 pt scale
 trust_t1 <- rnorm(n, mean = 3 + 0.05 * age, sd = 1)
@@ -56,21 +47,22 @@ plot(age, trust_t1)
 # - force 10 pt scale
 
 trust_t2 <- trust_t1 + 
-  (experimental_group == "positive") * 0.4 +
-  (experimental_group == "negative") * (!np_subscription) * -1.6+
-  (experimental_group == "negative") * np_subscription * -0.3 +
+  (experiment_group == "positive") * 0.4 +
+  (experiment_group == "negative") * (!np_subscription) * -1.6+
+  (experiment_group == "negative") * np_subscription * -0.3 +
   rnorm(n, mean = 0, sd = 1)
 trust_t2[trust_t2 < 1] <- 1
 trust_t2[trust_t2 > 10] <- 10
 
-plot(trust_t2 ~ experimental_group)
+plot(trust_t2 ~ experiment_group)
 
 # create tibble and randomize order
 d = tibble(
-    participant_id = 1:n,
+    id = 1:n,
     age = age,
     np_subscription = factor(np_subscription, labels = c("no", "yes")),
-    experimental_group = experimental_group,
+    news_consumption = news_consumption,
+    experiment_group = experiment_group,
     trust_t1 = trust_t1,
     trust_t2 = trust_t2
 )
@@ -88,23 +80,23 @@ t.test(trust_t1 ~ np_subscription, var.equal=TRUE, data = d)
 plot(trust_t1 ~ np_subscription, data = d)
 
 # variances not equal
-car::leveneTest(age ~ np_subscription, data = d)
+car::leveneTest(news_consumption ~ np_subscription, data = d)
 t.test(age ~ np_subscription, var.equal=FALSE, data = d)
 plot(age ~ np_subscription, data = d)
 
 # ANOVA
-aov(trust_t2 ~ experimental_group, data = d) |> summary()
-plot(trust_t2 ~ experimental_group, data = d)
+aov(trust_t2 ~ experiment_group, data = d) |> summary()
+plot(trust_t2 ~ experiment_group, data = d)
 
 # Regression with interaction
-m = lm(trust_t2 ~ experimental_group * np_subscription, data = d)
-plot_model(m, type = "pred", terms = c("experimental_group", "np_subscription"), show.values = TRUE)
+m = lm(trust_t2 ~ experiment_group * np_subscription, data = d)
+plot_model(m, type = "pred", terms = c("experiment_group", "np_subscription"), show.values = TRUE)
 
 dl <- d |> 
   pivot_longer(cols = c(trust_t1, trust_t2), names_to = "time", values_to = "trust") |>
   mutate(time = ifelse(time == "trust_t1", "t1", "t2"))
 
-ggplot(dl, aes(x = age, y = trust, color = experimental_group)) +
+ggplot(dl, aes(x = age, y = trust, color = experiment_group)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
   facet_wrap(~time)
