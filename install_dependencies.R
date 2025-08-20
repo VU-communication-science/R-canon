565142247df1f9770a9b4bc23730675de4551633
Kasper Welbers <kasperwelbers@gmail.com>
2025-08-20 08:47:29 +0200

    fixed renv stuff

--- install_dependencies.R
+++ install_dependencies.R
@@ -2,16 +2,17 @@
 ## Install renv and yaml. This allows using
 ## renv::dependencies(), which lists all the 
 ## packages referred to in all Quarto files.
-
-if (!require('renv', quietly = T)) install.packages('renv')
-if (!require('yaml', quietly = T)) install.packages('yaml')
+if (!length(find.package('renv', quiet = T))) install.packages('renv')
+if (!length(find.package('yaml', quiet = T))) install.packages('yaml')
   
 ## Get the unique package names
 deps <- renv::dependencies()
 udeps <- unique(deps$Package)
 
-udeps <- udeps[!udeps %in% c('renv','yaml')]
+cat("Book currently uses the following packages:\n", 
+    paste(paste('-', udeps), collapse='\n'))
 
 ## Install
 install.packages(udeps)
+
 
